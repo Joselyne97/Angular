e6d5f933234f81df3d43a685e0affb84abbf8e28
Joselyne97 <joselynejojo740@gmail.com>
2019-08-22 12:58:41 +0200

    angular

---
+++ angular/Goals/src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css']
+})
+export class GoalDetailComponent implements OnInit {
+
+  @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  // completeGoal(isComplete, index){
+  //   if (isComplete) {
+  //     this.goals.splice(index,1);
+  //   }
+  // }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
